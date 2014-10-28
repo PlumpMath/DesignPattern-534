519f57c202b9cc45e411fd9e1016f5b93871866e
wzfzlw <476862358@qq.com>
2014-10-28 16:03:31 +0800

    Signed-off-by: wzfzlw <476862358@qq.com>

--- DesignPatternSamples/src/com/command/demo02/Lucy.java
+++ DesignPatternSamples/src/com/command/demo02/Lucy.java
@@ -30,6 +30,27 @@ public class Lucy {
 		keybord.play();
 		
 		
+		/**
+		 * 可以发现 这个命令模式里
+		 * 真正会变化的  就是接收者的实现代码 ，以及Command实现类里面 调用接收者方法的
+		 * 那部分代码
+		 * 
+		 * 
+		 * 而客户和调用者 都是不用变化的  从而实现解耦
+		 *
+		 * 本质上的原因在于 客户端new出了AudioPlayer的实例 
+		 * 但是客户端不直接去调用AudioPlayer的方法
+		 * 
+		 * 而是由
+		 * Lucy 通过 KeyBord来执行Command
+		 * 然后Command来调用AudioPlayer的方法 
+		 *
+		 * Lucy---> keybord.play() --->Command.excute() --->AudioPlayer.play()
+		 * 
+		 * 可见 Lucy和 AudioPlayer是完全解耦的
+		 * 
+		 */
+		
 	}
 	
 	
